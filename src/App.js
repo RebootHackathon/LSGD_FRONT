3f78f2a4859537d1d0ad0e8dcc7fa7e3bef71a88
Rahul C <gorahul10@gmail.com>
2020-08-14 17:38:16 +0530

    working login

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Login from './Containers/Login';
 import MainPage from './Containers/MainPage';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
-        <Route path="/" component={Login}/>
-        <Route path="/mainpage" component={MainPage}/>
-         
+        <Switch>
+          
+          <Route path="/mainpage" component={MainPage}/>
+          <Route path="/" component={Login}/>
+        </Switch>
       </div>
      
     );
