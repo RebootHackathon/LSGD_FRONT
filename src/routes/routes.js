004e1aca2a456111db9e4fff25d1a0bbd2f424c5
DONATTOMOOKKEN <33897933+DONATTOMOOKKEN@users.noreply.github.com>
2020-08-15 00:48:54 +0530

    corrected on merge rahul

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import MainPage from '../Containers/MainPage';
 import ListGrant from '../Containers/ListGrant';
 import Login from '../Containers/Login';
+import ExpandCard from '../Components/CardView/ExpandCard';
 
 var routes = [
     { path: "/mainpage", name: "mainpage", component: MainPage },
     { path: "/listgrants", name: "listgrants", component: ListGrant },
+    { path: "/:id", name: "listgrants", component: ExpandCard },
     { path: "/", name: "login", component: Login },
   ];
 
