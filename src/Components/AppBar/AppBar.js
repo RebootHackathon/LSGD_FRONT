87060ba7c708e0f28cd2a1fdcf86ee7d2dd86146
Navaneeth <navaneethsaj@gmail.com>
2020-08-16 16:07:39 +0530

    done for now

--- src/Components/AppBar/AppBar.js
+++ src/Components/AppBar/AppBar.js
@@ -18,6 +18,7 @@ const AppBar = () => (
               <Nav className="mr-auto">
                   <Nav.Link href={'/LSGD_FRONT/mainpage'}>Home</Nav.Link>
                   <Nav.Link >About</Nav.Link>
+                  <Nav.Link href={'/'}>Login</Nav.Link>
                   <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                       <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                       <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
