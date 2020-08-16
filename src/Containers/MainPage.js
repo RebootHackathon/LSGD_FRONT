533463734fbd38a640bd31ce5aaf353145ac93cf
Navaneeth <navaneethsaj@gmail.com>
2020-08-16 12:58:08 +0530

    main page ui

--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -1,16 +1,42 @@
 import React,{Component} from 'react';
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
+import AadharCard from '../assets/aadharcard.png'
+import AppBar from "../Components/AppBar/AppBar";
 class MainPage extends Component{
-    onClickHandler=()=>{
-        // console.log(this.props);
-        this.props.history.push('/LSGD_FRONT/listgrants')
-    }
     render(){
         return(
-            <div>
-              Logged in
+            <div style={{width: '100%'}}>
+                <Container fluid>
+                    <Row>
+                        <AppBar/>
+                    </Row>
+                    <Row style={{marginTop: '1%'}}>
+                        <Col sm={12}>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={AadharCard} />
+                                <Card.Body>
+                                    <Card.Title>Get User Schemes</Card.Title>
+                                    <Card.Text>
+                                        Retrieve Schemes granted to a person by Aadhar
+                                    </Card.Text>
+                                    <Link to="/LSGD_FRONT/listgrants">
+                                        <Button block variant="primary">Go</Button>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
 
-              <button onClick={this.onClickHandler}>Go</button>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
