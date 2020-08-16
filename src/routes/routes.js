6909f2bca0ddcc24386e4317dd6efce3d40ad50e
Navaneeth <navaneethsaj@gmail.com>
2020-08-16 20:11:33 +0530

    Auto stash before merge of "master" and "origin/master"

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 import MainPage from '../Containers/MainPage';
 import ListGrant from '../Containers/ListGrant';
 import Login from '../Containers/Login';
-import ExpandCard from '../Components/CardView/ExpandCard';
 import HomePageVM from "../pages/HomePage/HomePageVM";
 import ApplyGrant from '../Containers/ApplyGrant';
 
@@ -9,10 +8,7 @@ var routes = [
     { path: "/LSGD_FRONT/mainpage", name: "mainpage", component: MainPage },
     { path: "/LSGD_FRONT/listgrants", name: "listgrants", component: ListGrant },
     { path: "/LSGD_FRONT/applygrant", name: "applygrant", component: ApplyGrant },
-
-
     { path: "/LSGD_FRONT/home", name: "home", component: HomePageVM },
-    { path: "/LSGD_FRONT/:id", name: "listgrants", component: ExpandCard },
     { path: "/LSGD_FRONT/", name: "login", component: Login },
     { path: "/", name: "login", component: Login },
 
