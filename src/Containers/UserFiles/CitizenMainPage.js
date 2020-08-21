b6e45ea47b7ca35895de02b70962faac774319a5
Rahul C <gorahul10@gmail.com>
2020-08-21 21:39:18 +0530

    mainpage citizen

---
+++ src/Containers/UserFiles/CitizenMainPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
+class CitizenMainPage extends Component {
+    render() {
+        return (
+            <div>
+            <Link to="/citizenviewallgrants">
+            <Button block variant="primary">View All grants</Button>
+        </Link>
+        <Link to="/citizenappliedgrant">
+            <Button block variant="primary">View applied Grants</Button>
+        </Link>
+        </div>
+        )}
+}
+
+export default CitizenMainPage;
